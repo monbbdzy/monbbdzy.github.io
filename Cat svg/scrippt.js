01349afc9ee76a1b7e0e3bce38056f11a40b862b
monbbdzy <nigina.rashidowa@gmail.com>
2025-02-21 15:30:46 +0400

    new

--- Cat svg/scrippt.js
+++
@@ -1,5 +0,0 @@
-let insideOfBridge = document.getElementById("inside")
-
-insideOfBridge.addEventListener("click", () => {
-	document.getElementById("textbox").innerText = "*hiss* Don't touch my TAIL!"
-})
---
+++ Catsvg/scrippt.js
@@ -0,0 +1,5 @@
+let insideOfBridge = document.getElementById("inside")
+
+insideOfBridge.addEventListener("click", () => {
+	document.getElementById("textbox").innerText = "*hiss* Don't touch my TAIL!"
+})
